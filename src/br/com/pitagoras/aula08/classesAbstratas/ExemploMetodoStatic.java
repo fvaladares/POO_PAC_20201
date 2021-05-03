29ed12914ae98e97e13b985733044a6bd685c3b9
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-05-03 19:49:22 -0300

    Início da aula do dia 03/05

---
+++ src/br/com/pitagoras/aula08/classesAbstratas/ExemploMetodoStatic.java
@@ -0,0 +1,8 @@
+package br.com.pitagoras.aula08.classesAbstratas;
+
+public class ExemploMetodoStatic {
+
+    static void print() {
+        System.out.println("Estamos invocando um método estático.");
+    }
+}
---
+++ src/br/com/pitagoras/aula08/classesAbstratas/ExemploMetodoStaticDemo.java
@@ -0,0 +1,4 @@
+package br.com.pitagoras.aula08.classesAbstratas;
+
+public class ExemploMetodoStaticDemo {
+}
